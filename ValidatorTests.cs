58156942f882e1167866b59f991666eccf88daac
Vinicius Lucena <vilucenasantos@gmail.com>
2020-12-14 21:02:41 -0300

    1

---
+++ ValidatorTests.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class ValidatorTests
+{
+	public ValidatorTests()
+	{
+	}
+}
---
+++ WordChainsTests/ValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace WordChain.UnitTests
+{
+	public class ValidatorTests
+	{
+		public ValidatorTests()
+		{
+		}
+
+		[Fact]
+		public void Should()
+		{
+			var expected = new List<string> { "dog, dot, cot" };
+			, "dog, dot, cot"
+			Validator.IsValid(current);
+
+			// Validator de fato
+
+			// Palavras Válidas
+		}
+	}
+}
+
