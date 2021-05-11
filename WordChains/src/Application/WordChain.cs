0cfb7285286e5c2a8f9b70374c644ece7db355eb
Vinicius Lucena <vilucenasantos@gmail.com>
2021-05-10 21:13:29 -0300

    1

---
+++ WordChains/src/Application/WordChain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+    public class WordChain
+    {
+        public List<string> Solver(string first, string last)
+        {
+            return null;
+        }
+    }
+}
