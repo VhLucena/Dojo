2c3c21d961af1d063ae36dda19ffe50d1cb6e68e
Rafael Bulsing <rafabulsing@gmail.com>
2021-05-10 21:30:50 -0300

    a

--- WordChains/tests/UnitTests/WordChainTests.cs
+++ WordChains/tests/UnitTests/WordChainTests.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Application;
 
 namespace UnitTests
 {
     public class WordChainTests
     {
-    }
+		private readonly WordChain _wordChain;
+
+		public WordChainTests()
+        {
+			var dictionary = List<string> { "cat", "dog" };
+			_wordChain = new WordChain();
+        }
+
+		[Fact(DisplayName = "Validator -> Should Consider Valid -> Valid Words")]
+		public void Should_ReturnListWithJustTheWord_When_FirstAndLastAreTheSameWord()
+		{
+			// Arrange
+			var input = "dog";
+
+			// Act / Assert
+			_validator.IsValidWords(validInput);
+		}
+	}
 }
